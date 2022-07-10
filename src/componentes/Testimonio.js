673fc62382977f29ec4274021f9ad7b82d3d27c9
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-07-10 15:28:31 -0600

    perfiles

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "../stylesheets/Testimonio.css";
 
-function Testimonio(){
+export function Testimonio(props){
+  
   return(
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio"
-      src={require("../imagenes/testimonio-emma.png")}
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
       alt="foto de emma"/>
+        <div className="contenedor-texto-testimonio">
+          <p className="encabezado-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+          <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+          <p className="texto-testimonio">{props.testimonio}</p>
+        </div>
     </div>
   );
 }
+
